33f554447d803215539a9d20736988e3946c09d3
Andre Soares Gomes <a.sgomes@cf-partners.com>
2025-09-13 11:27:25 -0300

    feat(signal-9999): added swagger

--- SignalRBackEnd/SignalR/Program.cs
+++ SignalRBackEnd/SignalR/Program.cs
@@ -9,10 +9,47 @@ builder.Services.AddJwtAuth(builder.Configuration);
 
 builder.Services.AddSignalR(options =>
 {
-    options.MaximumReceiveMessageSize = 1024 * 1024; 
-    options.StreamBufferCapacity = 10; 
+    options.MaximumReceiveMessageSize = 1024 * 1024;
+    options.StreamBufferCapacity = 10;
 });
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "SignalR API",
+        Version = "v1",
+        Description = "SignalR API with JWT Authentication"
+    });
+
+    // Configure JWT authentication for Swagger
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 // Serviços de domínio/autenticação
 builder.Services.AddSingleton<IRefreshTokenStore, InMemoryRefreshTokenStore>();
@@ -26,6 +63,12 @@ app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "SignalR API v1");
+        options.RoutePrefix = "swagger";
+    });
     app.MapOpenApi();
 }
 
